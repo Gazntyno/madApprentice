ca38cfce8bec804fb289913fdbef1746a0b6c0fb
Gazntyno <gazntyno404@gmail.com>
2023-02-27 14:01:32 -0500

    CRUD app Complete.
    Has simple Edit and delete functionality

--- soCRUD.js
+++ soCRUD.js
@@ -18,6 +18,7 @@ let formValidation = () => {
     } else {
         console.log("success");
         msg.innerHTML = "";
+        acceptData();
     }
 };
 
@@ -25,4 +26,25 @@ let data = {};
 let acceptData = () => {
     data["text"] = input.value;
     console.log(data);
+    createPost();
 };
+
+let createPost = () => {
+    posts.innerHTML += `<div>
+    <p>${data.text}</p>
+    <span class="options"> 
+    <i onCLick="editPost(this)" class="fas fa-edit"></i>
+    <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+    </span>
+    </div>`;
+    input.value = "";
+};
+
+let deletePost =  (e) => {
+    e.parentElement.parentElement.remove();
+}
+
+let editPost = (e) => {
+input.value = e.parentElement.previousElementSibling.innerHTML;
+e.parentElement.parentElement.remove();
+}
