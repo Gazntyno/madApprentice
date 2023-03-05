be954aa3e977c03b0d8a4f0090f9287d8f781d54
Gazntyno <gazntyno404@gmail.com>
2023-03-04 22:38:11 -0500

    Updated HTMl, CSS, and added JS

---
+++ todoCRUD.js
@@ -0,0 +1,71 @@
+let form = document.getElementById("form");
+let textInput = document.getElementById("textInput");
+let dateInput = document.getElementById("dateInput");
+let textarea = document.getElementById("textarea");
+let msg = document.getElementById("msg");
+let tasks = document.getElementById("tasks");
+let add = document.getElementById("add");
+
+form.addEventListener("submit", (e) =>
+{
+    e.preventDefault();
+    formValidation();
+});
+
+let formValidation = () => {
+    if (textInput.value === "") {
+        console.log("failure");
+        msg.innerHTML = "Task cannot be blank";
+    } else {
+        console.log("success");
+        msg.innerHTML = "";
+        acceptData();
+        add.setAttribute("data-bs-dismiss", "modal");
+        add.click();
+
+        (() => {
+            add.setsttribute("data=bs=dismiss", "");
+        })();
+    }
+};
+
+let data = [];
+
+let acceptData = () => {
+    data.push({
+        text: textInput.value,
+        date: dateInput.value,
+        description: textarea.value,
+    });
+
+    localStorage.setItem("data", JSON.stringify(data));
+
+    console.log(data);
+    createTasks();
+};
+
+let createTasks = () => {
+    tasks.innerHTML = "";
+    data.map((x,y) => {
+        return (tasks.innerHTML += 
+        <div id={y}>
+            <span class="fw-bold">${x.text}</span>
+            <span class="small text-secondary">${x.date}</span>
+            <p>${x.description}</p>
+    
+            <span class="options">
+              <i onClick= "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
+              <i onClick ="deleteTask(this);createTasks()" class="fas fa-trash-alt"></i>
+            </span>
+          </div>);
+    });
+
+    resetForm();
+};
+
+let resetForm = () => {
+    textInput.value = "";
+    dataInput.value = "";
+    textarea.value = "";
+};
+
