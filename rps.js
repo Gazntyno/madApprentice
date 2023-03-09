5c89855d92215b4e96574d1b90ad2bbe86271b44
Gazntyno <gazntyno404@gmail.com>
2023-03-09 02:17:27 -0500

    Cleaning up code

--- rps.js
+++ rps.js
@@ -4,33 +4,41 @@ const resultDisplay = document.getElementById("result");
 const possibleChoices = document.querySelectorAll("button");
 let userChoice;
 let computerChoice;
+let result;
 
-possibleChoices.foreach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+possibleChoices.foreach(possibleChoice => possibleChoice.addEventListener('click', (e) => 
+    {
     userChoice = e.target.id;
     userChoiceDisplay.innerHTML = userChoice;
 
-}));
+    }));
 
-function generateComputerChoice() {
+function generateComputerChoice() 
+{
     const randomNum = Math.floor(Math.random() * 3) + 1;
 
-    if (randomNum === 1) {
+    if (randomNum === 1) 
+    {
         computerChoice = 'rock';
     }
 
-    if (randomNum === 2) {
+    if (randomNum === 2) 
+    {
         computerChoice = 'paper';
     }
 
-    if (randomNum === 3) {
+    if (randomNum === 3) 
+    {
         computerChoice = 'scissors';
     }
 
     computerChoiceDisplay.innerHTML = computerChoice;
 }
 
-function getResult () {
-    if (computerChoice === userChoice){
+function getResult () 
+{
+    if (computerChoice === userChoice)
+    {
         result = 'its a draw!'
     }
 
