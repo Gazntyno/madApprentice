bb95e5bc9745ed18679c815592b4121d2d5a5c38
Gazntyno <gazntyno404@gmail.com>
2023-04-03 17:46:28 -0400

    A fortune teller programs
    Working on generating a random number at the moment.

---
+++ fortune.cpp
@@ -0,0 +1,19 @@
+// A fortune teller 
+
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+int main() 
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0,99);
+
+    int rannum = dis(gen);
+    std::cout << "Random Number: " << rannum << std::endl;
+
+    return 0;
+    
+}
