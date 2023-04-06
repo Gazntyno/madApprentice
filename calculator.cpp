e5be7dfbce5c4ca76f6c94ffe48cd84016fe519e
Gazntyno <gazntyno404@gmail.com>
2023-04-06 18:52:38 -0400

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -11,9 +11,8 @@ int main() {
    cout << "Enter calculation: ";
    cin >> num1 >> op >> num2 ;
 
-   //cout << "Enter two numbers:";
-   //cin >> num1 >> num2;
 
+// Checks for operator
    switch(op)
    {
     case '+':
@@ -27,7 +26,7 @@ int main() {
     case '*':
     cout << num1 * num2 << "\n";
     break;
-
+// Checks for errors 
     case '/':
     if(num2 == 0)
     {
