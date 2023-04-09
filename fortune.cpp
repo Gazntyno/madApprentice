898141f917ac1e7041d61f88d73fb927db0aac78
Gazntyno <gazntyno404@gmail.com>
2023-04-09 15:47:22 -0400

    Update fortune.cpp

--- fortune.cpp
+++ fortune.cpp
@@ -16,18 +16,32 @@ int main()
     std::cout << "Random Number: " << rannum << "\n";
     
     //Prints Fortune
-    if(rannum < 50)
-   {
-       std::cout << "The odds are in your favor";
-   }
-   else if(rannum > 50)
-   {
-       std::cout << "Not likely";
-   }
-   else{
-       std::cout << "Ask again later";
-   }
+    switch(rannum)
+    {
+        case 1:
+        std::cout << "The odds are in your favor\n";
+        break;
 
+         case 2:
+        std::cout << "It's possible\n";
+        break;
+
+         case 3:
+        std::cout << "You must wait and find out for yourself\n";
+        break;
+
+         case 4:
+        std::cout << "You already know the asnwer\n";
+        break;
+        
+         case 5:
+        std::cout << "Not likely\n";
+        break;
+
+        default: std::cout << "Future is undecided. Ask again later\n";
+
+    }
+    
     return 0;
     
 }
