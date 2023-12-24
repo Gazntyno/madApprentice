7ed2b0e1a8f8a8840419f51fd16561b132fefebc
Gazntyno <gazntyno404@gmail.com>
2023-12-24 11:51:45 -0500

    Configured Three.js
    Got Three.js working in this project

--- pseudoZyme/zymeScene.js
+++ pseudoZyme/zymeScene.js
@@ -1,14 +1,16 @@
-import * as THREE from 'three';
+import * as THREE from '../three.js-master/build/three.module.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
+//Window
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+//Object
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true} );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
